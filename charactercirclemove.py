0dcc0bb90bdce38dfd5d35a2ab356b09bccf3de3
roal518 <roalqwerty@gmail.com>
2023-09-18 12:23:08 +0900

    자연스럽게 도착
    지면에서 시작해서 멈추도록 변경했습니다.

--- charactercirclemove.py
+++ charactercirclemove.py
@@ -11,10 +11,10 @@ theta=360
 while(0<theta):
     clear_canvas_now()
     grass.draw_now(400,30)
-    character.draw_now(x+400,y+180)
+    character.draw_now(x+400,y+25)
     theta=theta-1
-    x=x+2*math.cos(theta/360*2*math.pi)
-    y=y+2*math.sin(theta/360*2*math.pi)
+    x=x-2*math.cos(theta/360*2*math.pi)
+    y=y-2*math.sin(theta/360*2*math.pi)
     delay(0.01)
 
 
